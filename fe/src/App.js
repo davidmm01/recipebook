e9a14ff32e549bec83f9eeeb2670fb8dda0b10d2
David Michieli <d.m.michieli@gmail.com>
2024-07-08 20:40:26 +1000

    get the cuisine buttons doing good stuff

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,61 +1,60 @@
-
 // App.js
 import React from "react";
 import "./App.css";
-import RecipeCard from "./components/RecipeCard"
-import Cuisines from "./components/Cuisines"
-import Descriptors from "./components/Descriptors"
+import RecipeCard from "./components/RecipeCard";
+import Cuisines from "./components/Cuisines";
+import Descriptors from "./components/Descriptors";
 
 class App extends React.Component {
-    // Constructor
-    constructor(props) {
-        super(props);
+  // Constructor
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            recipes: [],
-            DataisLoaded: false,
-        };
-    }
+    this.state = {
+      recipes: [],
+      DataisLoaded: false,
+    };
+  }
 
-    // ComponentDidMount is used to
-    // execute the code
-    componentDidMount() {
-        fetch("http://localhost:8080/recipes")
-            .then((res) => res.json())
-            .then((json) => {
-                this.setState({
-                    recipes: json,
-                    DataisLoaded: true,
-                });
-            });
-    }
+  // ComponentDidMount is used to
+  // execute the code
+  componentDidMount() {
+    fetch("http://localhost:8080/recipes")
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          recipes: json,
+          DataisLoaded: true,
+        });
+      });
+  }
 
-    render() {
-        const { DataisLoaded, recipes } = this.state;
-        if (!DataisLoaded)
-            return (
-                <div>
-                    <h1> Pleses wait some time.... </h1>
-                </div>
-            );
+  render() {
+    const { DataisLoaded, recipes } = this.state;
+    if (!DataisLoaded)
+      return (
+        <div>
+          <h1> Pleses wait some time.... </h1>
+        </div>
+      );
 
-        return (
-            <div>
-                <Cuisines />
-                <Descriptors />
-                <h2>Recipes</h2>
-                <div className="container">
-                    {recipes.map((recipe) => (
-                        <div className="recipe">
-                            <ol>
-                                <RecipeCard name={recipe.Name}/>
-                            </ol>
-                        </div>
-                    ))}
-                </div>
+    return (
+      <div>
+        <Cuisines />
+        <Descriptors />
+        <h2>Recipes</h2>
+        <div className="container">
+          {recipes.map((recipe) => (
+            <div className="recipe">
+              <ol>
+                <RecipeCard name={recipe.Name} />
+              </ol>
             </div>
-        );
-    }
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- fe/src/components/Cuisines.js
+++ fe/src/components/Cuisines.js
@@ -1,51 +1,77 @@
 import React from "react";
+import { useState } from "react";
+import "./cuisines.css";
+
+// used the following to help make the button group
+// https://dev.to/ramonak/react-how-to-create-a-custom-button-group-component-in-5-minutes-3lfd
 
 class Cuisines extends React.Component {
-    // Constructor
-    constructor(props) {
-        super(props);
+  // Constructor
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            cuisines: [],
-            DataisLoaded: false,
-        };
+    this.state = {
+      cuisines: [],
+      DataisLoaded: false,
+      selectedCuisineKey: -1,
+    };
+  }
+
+  setSelectedCuisineKey = (key) => {
+    console.log("eat my eggs, key=", key);
+    // on re-click of already selected cuisine, clear selection
+    if (this.state.selectedCuisineKey == key) {
+      key = -1;
     }
+    this.setState({ selectedCuisineKey: key });
+  };
 
-    // ComponentDidMount is used to
-    // execute the code
-    componentDidMount() {
-        fetch("http://localhost:8080/cuisines")
-            .then((res) => res.json())
-            .then((json) => {
-                this.setState({
-                    cuisines: json,
-                    DataisLoaded: true,
-                });
-            });
-    }
+  // ComponentDidMount is used to
+  // execute the code
+  componentDidMount() {
+    fetch("http://localhost:8080/cuisines")
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          cuisines: json,
+          DataisLoaded: true,
+        });
+      });
+  }
 
-    render() {
-        const { DataisLoaded, cuisines } = this.state;
-        if (!DataisLoaded)
-            return (
-                <div>
-                    <h1> Pleses wait some time.... </h1>
-                </div>
-            );
+  render() {
+    const { DataisLoaded, cuisines, selectedCuisineKey } = this.state;
+    if (!DataisLoaded)
+      return (
+        <div>
+          <h1> Pleses wait some time.... </h1>
+        </div>
+      );
 
-        return (
-            <div className="container">
-                <h2>Cuisines</h2>
-                {cuisines.map((cuisine) => (
-                    <div>
-                        <ol>
-                            <button>{cuisine.Name} ({cuisine.Count})</button>
-                        </ol>
-                    </div>
-                ))}
-            </div>
-        );
-    }
+    return (
+      <div className="container">
+        <h2>Cuisines</h2>
+        {cuisines.map((cuisine, i) => (
+          <div>
+            <ol>
+              <button
+                key={i}
+                name={cuisine.Name}
+                onClick={() => this.setSelectedCuisineKey(i)}
+                className={
+                  i === this.state.selectedCuisineKey
+                    ? "customButton active"
+                    : "customButton"
+                }
+              >
+                {cuisine.Name} ({cuisine.Count})
+              </button>
+            </ol>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Cuisines;
